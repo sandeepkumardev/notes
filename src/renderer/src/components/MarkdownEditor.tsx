8a879a28649c836869dff13bd98f1174db6d082d
Sandeep Kumar <sandeepypb@gmail.com>
2024-01-18 17:03:29 +0530

    🐛 Fix: fix height and autofocus

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -14,13 +14,14 @@ export const MarkdownEditor = () => {
 
   return (
     <MDXEditor
+      autoFocus={selectedNote && true}
       ref={eidtorRef}
       key={selectedNote.title}
       markdown={selectedNote.content}
       onChange={handleAutoSaving}
       onBlur={handleBlur}
       plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin()]}
-      contentEditableClassName="outline-none max-w-none text-lg px-4 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
+      contentEditableClassName="outline-none max-w-none min-h-[calc(100vh-50px)] text-lg px-4 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   )
 }
